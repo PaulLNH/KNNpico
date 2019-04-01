795fc2910f63eb3a9f8fd42761e16f5f8e775007
plaird730 <plaird730@gmail.com>
2019-03-31 20:49:56 -0400

    Updated algorithm to factor in pythagorean theorem in the distance equation

--- score.js
+++ score.js
@@ -1,6 +1,6 @@
 let outputs = [];
 // The 'K' in K-Nearest Neightbor, or the top results to look at
-const k = 3;
+// const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
@@ -8,21 +8,30 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  const testSetSize = 10;
+  const testSetSize = 100;
   const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 
-  let numberCorrect = 0;
-  for (let i = 0; i < testSet.length; i++) {
-    const bucket = knn(trainingSet, testSet[i][0]);
-    if (bucket === testSet[i][3]) {
-      numberCorrect++;
-    }
-  }
+  // let numberCorrect = 0;
+  // for (let i = 0; i < testSet.length; i++) {
+  //   const bucket = knn(trainingSet, testSet[i][0]);
+  //   if (bucket === testSet[i][3]) {
+  //     numberCorrect++;
+  //   }
+  // }
 
-  console.log('Accuracy: ', numberCorrect / testSetSize);
+  _.range(1, 20).forEach(k => {
+    // Refactored if statement in lodash:
+    const accuracy = _.chain(testSet)
+      .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+      .size()
+      .divide(testSetSize)
+      .value();
+
+    console.log(`Accuracy for K of ${k} is: ${accuracy}`);
+  });
 }
 
-function knn(data, point) {
+function knn(data, point, k) {
   // Start a lodash chain and pass in our outputs arrya
   return (
     _.chain(data)
@@ -51,8 +60,25 @@ function knn(data, point) {
 }
 
 // Helper function to get the absolute value of the points
+// This calcuates a linear distance
+// function linear_distance(pointA, pointB) {
+//   return Math.abs(pointA - pointB);
+// }
+
+// Distance between points using Pythagorean Theorem
+// NOTE:  Easy way to test Pythagorean Theorem
+//        3, 4, 5 triangle - if one side is 3, the other is 4 the last will be 5
+// const pointA = [1, 1];
+// const pointB = [4, 5];
+// 1 - 4 = -3 && 1 - 5 = -4 so the hypotenuse will be 5
 function distance(pointA, pointB) {
-  return Math.abs(pointA - pointB);
+  return (
+    _.chain(pointA)
+      .zip(pointB)
+      .map(([a, b]) => (a - b) ** 2)
+      .sum()
+      .value() ** 0.5
+  );
 }
 
 function splitDataset(data, testCount) {
